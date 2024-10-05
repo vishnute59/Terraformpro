191d9a0d230692cfb52afdf9f586142ed4f993ef
vishnute59 <102622085+vishnute59@users.noreply.github.com>
2024-10-06 03:05:40 +0530

    Update vpc.tf

--- terraform/vpc.tf
+++ terraform/vpc.tf
@@ -2,7 +2,7 @@ data "aws_availability_zones" "azs" {}
 module "myapp-vpc" {
   source          = "terraform-aws-modules/vpc/aws"
   version         = "3.19.0"
-  name            = "myapp-vpc1"
+  name            = "myapp-vpc"
   cidr            = var.vpc_cidr_block
   private_subnets = var.private_subnet_cidr_blocks
   public_subnets  = var.public_subnet_cidr_blocks
